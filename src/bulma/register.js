a6e32ea537a458e153436430c43187833b65e0a2
reza <raftx24@gmail.com>
2020-11-17 20:26:50 +0330

    fixes navbar

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -3,4 +3,4 @@ import App from '@enso-ui/ui/src/core/app';
 import IO from './components/navbar/IO.vue';
 
 Vue.component('navbar-io', IO);
-App.registerSettingsItem('navbar-io', 200);
+App.registerNavbarItem('navbar-io', 200);
