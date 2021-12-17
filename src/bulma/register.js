b80030f0012511bf6ef02cc99d741abf38144877
Mihai <mihai.ocneanu@gmail.com>
2021-12-17 17:05:12 +0200

    wip

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,6 +1,4 @@
-import Vue from 'vue';
 import App from '@enso-ui/ui/src/core/app';
 import IO from './components/navbar/IO.vue';
 
-Vue.component('navbar-io', IO);
-App.registerNavbarItem('navbar-io', 200);
+App.registerNavbarItem('navbar-io', IO, 200);
